0425f9182890a01260918f4d2f9ef6a2b9f3d5ec
Danster4 <86613317+Danster4@users.noreply.github.com>
2021-10-25 12:49:11 -0400

    add employee test and test suite passes, beginning structure to index.js

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -1,14 +1,9 @@
-// Employee parent class with following properties and methods
-// name
-// id
-// email
-// getName()
-// getId()
-// getEmail()
-// getRole() => returns Employee
+const Employee = require("../lib/Employee");
 
+test('tests if employee is a new object created', () => {
+  const employee = new Employee('John', 14, 'john@company.com')
 
-// other three classes will extend Employee
-
-
-// Add validation that input is in the correct format
+  expect(employee.name).toEqual(expect.any(String));
+  expect(employee.id).toEqual(expect.any(Number));
+  expect(employee.email).toEqual(expect.any(String));
+})
---
+++ index.js
@@ -0,0 +1,96 @@
+const inquirer = require('inquirer');
+const fs = require('fs');
+const { writeFile, copyFile } = require('./utils/generate-site.js');
+const generatePage = require('./src/page-template.js')
+const Employee = require('./lib/Employee');
+const Manager = require("./lib/Manager");
+const Engineer = require("./lib/Engineer");
+const Intern = require("./lib/Intern");
+
+// empty array for number of employees created
+employeeBucket = [];
+
+const managerQuestions = [
+  {
+    type: 'input',
+    name: 'name',
+    message: 'What is your Manager\'s name?',
+    validate: managerNameInput => {
+      if (managerNameInput) {
+        return true;
+      } else {
+        console.log("Please enter your manager\'s name!");
+        return false;
+      }
+    }
+  },
+  {
+    type: 'input',
+    name: 'id',
+    message: 'Enter your team manager\'s employee ID',
+    validate: idInput => {
+      if (idInput) {
+        return true;
+      } else {
+        console.log("Please enter your team manager\'s employee ID!");
+        return false;
+      }
+    }
+  },
+  {
+    type: 'input',
+    name: 'email',
+    message: 'Enter your team manager\'s email address',
+    validate: emailInput => {
+      if (emailInput) {
+        return true;
+      } else {
+        console.log("Please enter your team manager\'s email address!");
+        return false;
+      }
+    }
+  },
+]
+
+const employeeQuestions = []
+
+const addManager = async() => {
+  const result = await inquirer.prompt(managerQuestions)
+  
+  const managerOccurance = new Manager(
+    result.name,
+    result.id,
+    result.email,
+    result.officeNumber
+  );
+  employeeBucket.push(managerOccurance);
+  addEmployee();
+};
+
+const addEmployee = async() => {
+  const result = await inquirer.prompt(employeeQuestions)
+  .then(function(result) {
+    switch (result.addEmployee) {
+      case "Yes, please add an Engineer to my team":
+        addEngineer();
+        break;
+
+      case "Yes, please add an Intern to my team":
+      addIntern();
+      break;
+
+      case "No, there are no more team members to add":
+      generatePage();
+      break;
+    }
+  })
+}
+
+
+
+
+
+
+
+// call addManager function to start application
+addManager();
